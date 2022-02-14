c1f8fabd9b761ba96ddcf76ec245c8dc8fc2b9ab
rusty619 <rusty6916@gmail.com>
2022-02-14 16:48:24 -0500

    work on JS

---
+++ script.js
@@ -0,0 +1,5 @@
+let hello = document.getElementById('hello');
+hello.innerHTML = "Hello.";
+
+let intro = document.getElementById('intro')
+intro.textContent = "I am just a beginner programmer. I love to learn and get better with my skills everyday.";
