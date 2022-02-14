9ff3b8482fa17a41e30e3ddaf4df4e33741bb4f0
rusty619 <rusty6916@gmail.com>
2022-02-14 17:02:40 -0500

    onMouseout and OnMouseOver Function developed

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ hello.innerHTML = "Hello.";
 
 let intro = document.getElementById('intro')
 intro.textContent = "I am just a beginner programmer. I love to learn and get better with my skills everyday.";
+
+function rockyMountain(x){
+    x.src="rocky Mountains.jpeg";
+}
+
+function mountain(x){
+    x.src="mountain.png";
+}
